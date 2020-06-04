980438a404b9eadccd75af5e20c7a07fc73340ad
Sascha van Os <52345349+Sedekil@users.noreply.github.com>
2020-06-04 21:34:06 +0200

    Add files via upload

---
+++ backend/server.js
@@ -0,0 +1,34 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const bcrypt = require('bcrypt-nodejs');
+const cors = require('cors');
+const knex = require('knex');
+
+const register = require('./controllers/register');
+const signin = require('./controllers/signin');
+const profile = require('./controllers/profile');
+const image = require('./controllers/image');
+
+const db = knex({
+    client: 'pg',
+    connection: {
+    connectionString : process.env.DATABASE_URL,
+    ssl: true,
+    }
+});
+
+const app = express();
+
+app.use(bodyParser.json());
+app.use(cors())
+
+app.get('/', (req, res) => { res.send('itt is working!') })
+app.post('/signin', signin.handleSignin(db, bcrypt))
+app.post('/register', register.handleRegister(db, bcrypt))
+app.get('/profile/:id', profile.handleProfileGet(db))
+app.put('/image', image.handleImage(db))
+app.post('/imageurl', image.handleApiCall())
+
+app.listen(process.env.PORT || 3001, () => {
+    console.log(`app is running on port ${process.env.PORT}`);
+})
